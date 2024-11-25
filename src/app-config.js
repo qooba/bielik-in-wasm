791b7e7f9b15c4dd75d4bee1970589e53d097b60
qooba <dev@qooba.net>
2024-11-26 00:21:53 +0100

    fix 11b

--- src/app-config.js
+++ src/app-config.js
@@ -6,7 +6,7 @@ export default {
 			"model_lib": "https://blog.qooba.net/bielik-in-wasm/binary-mlc-llm-libs/Bielik-7B-Instruct-v0.1-q4f32_1-webgpu.wasm"
 		},
 		{
-			"model": "http://localhost:5173/stories-thumbellama//Bielik-11B-v2.3-Instruct-q4f32_1-MLC/",
+			"model": "https://huggingface.co/onceuponai/Bielik-11B-v2.3-Instruct-q4f32_1-MLC/",
 			"model_id": "Bielik-11B-v2.3-Instruct-q4f32_1",
 			"model_lib": "https://blog.qooba.net/bielik-in-wasm/binary-mlc-llm-libs/Bielik-11B-v2.3-Instruct-q4f32_1-webgpu.wasm"
 		}
