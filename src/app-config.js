cc0f1b03bc5a3dd9ce48b2e3f7da1882e4311bee
qooba <dev@qooba.net>
2024-11-25 01:31:52 +0100

    change config

--- src/app-config.js
+++ src/app-config.js
@@ -3,12 +3,12 @@ export default {
 		{
 			"model": "https://huggingface.co/onceuponai/Bielik-7B-Instruct-v0.1-q4f32_1-MLC/",
 			"model_id": "Bielik-7B-Instruct-v0.1-q4f32_1",
-			"model_lib": "http://localhost:5173/stories-thumbellama/binary-mlc-llm-libs/Bielik-7B-Instruct-v0.1-q4f32_1-webgpu.wasm"
+			"model_lib": "https://blog.qooba.net/bielik-in-wasm/binary-mlc-llm-libs/Bielik-7B-Instruct-v0.1-q4f32_1-webgpu.wasm"
 		},
 		{
 			"model": "http://localhost:5173/stories-thumbellama//Bielik-11B-v2.3-Instruct-q4f32_1-MLC/",
 			"model_id": "Bielik-11B-v2.3-Instruct-q4f32_1",
-			"model_lib": "http://localhost:5173/stories-thumbellama/binary-mlc-llm-libs/Bielik-11B-v2.3-Instruct-q4f32_1-webgpu.wasm"
+			"model_lib": "https://blog.qooba.net/bielik-in-wasm/binary-mlc-llm-libs/Bielik-11B-v2.3-Instruct-q4f32_1-webgpu.wasm"
 		}
 	],
 	"use_web_worker": true
